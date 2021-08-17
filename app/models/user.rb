8b03b84f70507000dd7668ee80401cddfe5a3591
Aniruddha Agrawal <aniruddhaagr12@gmail.com>
2021-08-17 13:00:38 +0530

    Added tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
    attr_accessor :password
+
+   has_many :tokens
+
    validates :first_name, :last_name, :email, presence: true
    validates :password, presence: true, on: :create
    validates_length_of :password, :minimum => 5
